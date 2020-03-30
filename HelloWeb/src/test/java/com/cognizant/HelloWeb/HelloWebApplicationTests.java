b9074c2e71a7437d56158b8371c291c14fc4b1b2
AnkanaBasu <62287258+AnkanaBasu@users.noreply.github.com>
2020-03-30 09:34:34 +0530

    Add files via upload

---
+++ HelloWeb/src/test/java/com/cognizant/HelloWeb/HelloWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.HelloWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
